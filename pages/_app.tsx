f805e43eda360e11e9951f892d25ff52351e2e35
Lee <zhoujianpingls@outlook.com>
2025-07-20 22:27:05 -0700

    added English and French version

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { appWithTranslation } from 'next-i18next'
-import type { AppProps } from 'next/app'
-import '../src/app/globals.css'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default appWithTranslation(MyApp)
